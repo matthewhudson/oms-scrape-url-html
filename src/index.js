34686858d455db7e2539ac2ef8c63c87edcc1e90
Matthew Hudson <matthud@gmail.com>
2019-10-04 16:40:56 -0400

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,26 @@
+#!/usr/bin/env node
+
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const handler = require("./handler");
+
+app.use(bodyParser.json());
+
+const port = process.env.PORT || 8080;
+
+app.post("/scrape", async (req, res) => {
+  const { url, session = null } = req.body;
+
+  // @TODO: This handler should be the main responsibility in this file,
+  // handle definition in export like lambda-api, or elsewhere
+  const html = await handler({ url, session });
+
+  res.json({ html: html.html });
+});
+
+app.get("/health", (req, res) => res.send("OK"));
+
+app.listen(port, () =>
+  console.log(`Listening on localhost: http://localhost:${port}`)
+);
